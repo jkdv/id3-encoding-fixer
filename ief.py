255e986f398fd7d58fa508a5e89d98711a96f373
Heetae Kim <jason.heetae.kim@gmail.com>
2016-05-03 12:20:31 -0700

    Fixed bug being no arguments

--- ief.py
+++ ief.py
@@ -9,7 +9,10 @@ def main():
     parser.add_argument('-R', action='store_true', dest='recursive', help='Recursively fix all under the directory')
     parser.add_argument('path', nargs='?', metavar='<PATH>', help='Path to files or a directory')
     args = parser.parse_args()
-    convert.fix_save_batch(args.path, args.recursive)
+    try:
+        convert.fix_save_batch(args.path, args.recursive)
+    except convert.ConvertException:
+        parser.print_usage()
 
 
 if __name__ == '__main__':
